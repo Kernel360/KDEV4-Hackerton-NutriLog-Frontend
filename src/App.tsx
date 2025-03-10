5bc0a01b6c73d6a4af14c5c6502190919fac6233
an hyeonjin <hyeonjinan096@naver.com>
2025-03-11 01:44:24 +0900

    chore: UI라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <div className="flex h-screen w-screen items-center justify-center gap-10">
         <div
           id="layout-Root"
-          className="relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col text-white shadow-xl"
+          className="relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col shadow-xl"
         >
           <Outlet />
           {/* <Footer /> */}
