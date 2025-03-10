720614c22896cb7d4be0ec39e466ea62da5b5c38
an hyeonjin <hyeonjinan096@naver.com>
2025-03-11 02:33:50 +0900

    fix: login페이지에서 Footer 컴포넌트 안보이게 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 function App() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+
   return (
     <>
-<div className="flex h-screen w-screen items-center justify-center gap-10">
-  <div
-    id="layout-Root"
-    className="relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col shadow-xl"
-  >
-    <div className="flex-grow overflow-y-auto">
-      <Outlet />
-    </div>
-    <Footer />
-  </div>
-</div>
+      <div className="flex h-screen w-screen items-center justify-center gap-10">
+        <div
+          id="layout-Root"
+          className="relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col shadow-xl"
+        >
+          <div className="flex-grow overflow-y-auto">
+            <Outlet />
+          </div>
+          {/* /login 경로가 아니면 Footer를 렌더링 */}
+          {!isLoginPage && <Footer />}
+        </div>
+      </div>
     </>
   );
 }
