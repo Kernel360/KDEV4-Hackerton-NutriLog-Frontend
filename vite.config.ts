71c9c2558288f89294e266110d15e36f9bbc61b2
an hyeonjin <hyeonjinan096@naver.com>
2025-03-10 23:45:14 +0900

    fix: tailwind 설정 수정 #3

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import svgr from '@svgr/rollup';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "@svgr/rollup";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths(), svgr(), tailwindcss()],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  css: {
+    postcss: "./postcss.config.cjs",
+  },
 });
