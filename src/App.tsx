8b5ed220243756de3d5083df6b8cd7ebafb2c1c4
an hyeonjin <hyeonjinan096@naver.com>
2025-03-11 02:24:58 +0900

    style: Footer컴포넌트 위치 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Outlet } from "react-router-dom";
-import Footer from "./components/layout/Footer"
+import Footer from "./components/layout/Footer";
 function App() {
   return (
     <>
-      <div className="flex h-screen w-screen items-center justify-center gap-10">
-        <div
-          id="layout-Root"
-          className="relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col shadow-xl"
-        >
-          <Outlet />
-          <Footer />
-        </div>
-      </div>
+<div className="flex h-screen w-screen items-center justify-center gap-10">
+  <div
+    id="layout-Root"
+    className="relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col shadow-xl"
+  >
+    <div className="flex-grow overflow-y-auto">
+      <Outlet />
+    </div>
+    <Footer />
+  </div>
+</div>
     </>
   );
 }
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { Calendar, Pill, User } from "lucide-react";
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-white shadow-lg flex justify-between items-center p-4 px-12">
+    <div className="bg-white shadow-lg flex justify-between items-center p-4 px-12">
       <div className="flex-1 text-center">
         <Link to="/">
           <Calendar className="w-6 h-6 mx-auto" />
@@ -12,10 +12,12 @@ const Footer = () => {
 
       <div className="flex-1 text-center relative">
         <div className="w-16 h-16 bg-sky-500 rounded-full flex justify-center items-center mx-auto relative">
-          <Pill
-            className="text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-            size={32} 
-          />
+          <Link to="/form">
+            <Pill
+              className="text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+              size={32}
+            />
+          </Link>
         </div>
       </div>
 
