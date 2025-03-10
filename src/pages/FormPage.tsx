9dd79ae3a282aa8b2e9413750a918268d1489892
an hyeonjin <hyeonjinan096@naver.com>
2025-03-10 20:34:24 +0900

    feat: 페이지 라우팅 구현 #1

---
+++ src/pages/FormPage.tsx
@@ -0,0 +1,5 @@
+const FormPage = () => {
+  return <h2>Welcome to the Form Page!</h2>;
+}
+
+export default FormPage;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,5 @@
+const MyPage = () => {
+  return <h2>Welcome to the Login Page!</h2>;
+}
+
+export default MyPage;
