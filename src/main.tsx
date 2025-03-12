e20d8b15052d64aa818808bb840c2a917469883a
devRonPark <pbc9236@gmail.com>
2025-03-12 10:50:39 +0900

    feat: 서비스 워커 등록 및 로그인ㅇ ㅕ부 확인 후 FCM 토큰 발급 처리

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,9 @@ import FormPage from "./pages/FormPage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import MyPage from "./pages/MyPage.tsx";
 import OAuthRedirectRoute from "./pages/OAuthRedirectRoute.tsx";
+import { registerServiceWorker } from "./services/registerServiceWorker.ts";
+import { handleAllowNotification } from "./services/notificationPermission.ts";
+import { useAuthStore } from "./store/useAuthStore.ts";
 
 const router = createBrowserRouter([
   {
@@ -55,3 +58,14 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router} />
 );
+
+// 서비스 워커 등록
+registerServiceWorker();
+
+// 로그인 여부 체크하고 FCM 토큰 없는 경우 FCM 토큰 발급 처리
+const { accessToken } = useAuthStore.getState();
+if (accessToken && !localStorage.getItem("fcmToken")) {
+  (async () => {
+    await handleAllowNotification();
+  })();
+}
